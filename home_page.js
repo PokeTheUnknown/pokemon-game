8d61588ff207298480dafd0daf3e9a57c0b3a46d
Jacemick <151991389+Jacemick@users.noreply.github.com>
2024-07-09 02:19:22 +0000

    tyjkk

--- home_page.js
+++ home_page.js
@@ -1,12 +1,19 @@
-  function navigateToLink(url) {
+window.onload = function() {
+    var backgroundMusic = document.getElementById("backgroundMusic");
+    backgroundMusic.play();
+};
+
+function navigateToLink(url) {
     var buttonSound = document.getElementById("buttonSound");
+    var backgroundMusic = document.getElementById("backgroundMusic");
     buttonSound.play();
-  
+    backgroundMusic.pause();
+    backgroundMusic.currentTime = 0; // reset the music to the begenning
+
     // fading
     document.querySelector('.container').classList.add('fading');
   
     setTimeout(function() {
       window.location.href = url;
     }, 500); // the fading delay time
-  }
-  
+}
--- player_name.js
+++ player_name.js
@@ -10,7 +10,24 @@ function navigateToLink(url) {
 }
 
 // Combined function to save input and navigate to another page
+// Function to save the input value to local storage
+function saveInput() {
+    const inputValue = document.getElementById('userInput').value;
+    localStorage.setItem('userInput', inputValue);
+}
+
+// Function to navigate to another page
+function navigateToLink(url) {
+    window.location.href = url;
+}
+
+// Combined function to save input and navigate to another page
 function saveAndNavigate() {
+    var buttonSound = document.getElementById("buttonSound");
+    buttonSound.play();
     saveInput();
-    navigateToLink('day1_page.html');
+
+    setTimeout(function() {
+        navigateToLink('day1_page.html');
+    }, 500);
 }
